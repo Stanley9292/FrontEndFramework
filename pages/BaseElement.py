661340bc735b97c94a4e86596e2a37ad851e554e
Stanley9292 <stanalexandrugeorge@gmail.com>
2020-11-28 12:54:23 +0200

    Added scrollIntoView and moveToElement options.

--- pages/BaseElement.py
+++ pages/BaseElement.py
@@ -1,15 +1,25 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 class BaseElement(object):
     def __init__(self, driver, locator):
         self.driver = driver
         self.locator = locator
 
-
         self.web_element = None
         self.find()
+
+    def moveToElement(self):
+        element = WebDriverWait(
+            self.driver, 10).until(EC.visibility_of_element_located(
+                locator=self.locator
+            ))
+        self.web_element = element
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element)
+        return None
     
     def find(self):
         
@@ -18,6 +28,7 @@ class BaseElement(object):
                 locator=self.locator
             ))
         self.web_element = element
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
         return None
 
     def click(self):
