1ac1a8e34fbdd2ad8ffa4631013b5319990011fe
Stanley9292 <stanalexandrugeorge@gmail.com>
2020-11-28 12:05:57 +0200

    Added new test file.

---
+++ test_textboxpage.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from pages.BaseElement import BaseElement
+from pages.TextBoxPage import TextBoxPage
+
+# Test setup
+browser = webdriver.Chrome()
+
+
+# 
+page = TextBoxPage(driver=browser)
+page.go() 
+assert page.submitBtn.text == 'Submit'
+page.submitBtn.click()
+browser.quit()
+
