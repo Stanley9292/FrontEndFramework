ff4275edd87813dbc6a5a64f8928e45f6e301113
Stanley9292 <stanalexandrugeorge@gmail.com>
2020-12-01 00:02:28 +0200

    Added new tests.

--- tests/test_textboxpage.py
+++ tests/test_textboxpage.py
@@ -12,19 +12,54 @@ class TextBoxPageTests(unittest.TestCase):
         self.browser = webdriver.Chrome()
         self.page = TextBoxPage(driver=self.browser)
         self.page.go()
-        time.sleep(3)
-
+        
     @mark.submit
     @mark.smoke
     def test_submitButton(self):
         actual =  self.page.submitBtn.text
         self.assertEqual(actual, 'Submit')
 
-        self.page.submitBtn.find()
+    @mark.label
+    def test_labels(self):
+        name = self.page.nameLabel.text
+        self.assertEqual(name, 'Full Name')
+
+        email = self.page.emailLabel.text
+        self.assertEqual(email, 'Email')
+
+        cAdress = self.page.currentAdrLabel.text
+        self.assertEqual(cAdress, 'Current Address')
+
+        pAdress = self.page.permanentAdrLabel.text
+        self.assertEqual(pAdress, 'Permanent Address')
+
+    @mark.input
+    def test_input_fields(self):
+        testName = 'Stan Alexandru'
+        testEmail = 'stanalexandru@gmail.com'
+        testCurrentAdr = 'Nerva Traian'
+        testPermanentAdr = 'Baba Novac'
+
+        self.page.nameInput.input_text(testName)
+        self.page.emailInput.input_text(testEmail)
+        self.page.currentAdrInput.input_text(testCurrentAdr)
+        self.page.permanentAdrInput.input_text(testPermanentAdr)
+
         self.page.submitBtn.click()
 
-    def test_labels(self):
-        pass
+        actualName = self.page.nameOutput.text
+        self.assertEqual(actualName, 'Name:Stan Alexandru')
+
+        actualEmail = self.page.emailOutput.text
+        self.assertEqual(actualEmail, 'Email:stanalexandru@gmail.com')
+        
+        actualCurrentAdr = self.page.currentAdrOutput.text
+        self.assertEqual(actualCurrentAdr, 'Current Address :Nerva Traian')
+
+        actualPermanentAdr = self.page.permanentAdrOutput.text
+        self.assertEqual(actualPermanentAdr, 'Permananet Address :Baba Novac')
+
+        # import pdb; pdb.set_trace()
 
     def teardown(self):
         self.browser.quit()
