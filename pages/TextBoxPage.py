d99c8bb73aa071906a8922c18b1b9abfb8da2b52
Stanley9292 <stanalexandrugeorge@gmail.com>
2020-11-30 23:59:55 +0200

    Added new elements for textBoxPage.

--- pages/TextBoxPage.py
+++ pages/TextBoxPage.py
@@ -14,3 +14,99 @@ class TextBoxPage(BasePage):
             driver=self.driver,
             locator = locator
         )
+
+    @property
+    def nameLabel(self):
+        locator = Locator(by = By.ID, value = 'userName-label')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def emailLabel(self):
+        locator = Locator(by = By.ID, value = 'userEmail-label')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def currentAdrLabel(self):
+        locator = Locator(by = By.ID, value = 'currentAddress-label')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def permanentAdrLabel(self):
+        locator = Locator(by = By.ID, value = 'permanentAddress-label')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def nameInput(self):
+        locator = Locator(by = By.ID, value = 'userName')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def emailInput(self):
+        locator = Locator(by = By.ID, value = 'userEmail')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def currentAdrInput(self):
+        locator = Locator(by = By.ID, value = 'currentAddress')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def permanentAdrInput(self):
+        locator = Locator(by = By.ID, value = 'permanentAddress')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def nameOutput(self):
+        locator = Locator(by = By.XPATH, value = '//p[@id="name"]')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def emailOutput(self):
+        locator = Locator(by = By.XPATH, value = '//p[@id="email"]')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def currentAdrOutput(self):
+        locator = Locator(by = By.XPATH, value = '//p[@id="currentAddress"]')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
+
+    @property
+    def permanentAdrOutput(self):
+        locator = Locator(by = By.XPATH, value = '//p[@id="permanentAddress"]')
+        return BaseElement(
+            driver=self.driver,
+            locator = locator
+        )
